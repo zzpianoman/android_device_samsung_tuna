5349df57366ab9e60a5521652e7a423f25159588
Kyle Repinski <repinski23@gmail.com>
2015-01-29 14:30:48 -0600

    Sync SELinux policy with Ziyan's tree.
    I'm sure he'll add more later, this is just to make it easy to merge then.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -145,16 +145,10 @@ BOARD_SEPOLICY_DIRS += \
 BOARD_SEPOLICY_UNION += \
 	genfs_contexts \
 	file_contexts \
-	bluetooth.te \
-	fRom.te \
 	init.te \
 	mediaserver.te \
 	pvrsrvinit.te \
-	rild.te \
-	sdcardd.te \
-	servicemanager.te \
-	system_server.te \
-	zygote.te
+	rild.te
 
 # TWRP
 DEVICE_RESOLUTION := 720x1280
