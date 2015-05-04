059789d0f69204614394be5c38d1b93543c4e854
Justin M <zzpianoman@gmail.com>
2015-05-04 07:31:22 -0400

    Toolchains: Build kernel with GCC 4.7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,7 +53,7 @@ BOARD_KERNEL_BASE := 0x80000000
 # BOARD_KERNEL_CMDLINE :=
 
 # Custom Toolchains
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-linaro-4.7
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
 TARGET_GCC_VERSION_EXP := linaro-4.8
 
 # Inline kernel building
