caa47ecdc3be9b2dd9645dd8f4a6cc64630fce21
Justin M <zzpianoman@gmail.com>
2016-02-03 22:53:08 -0500

    audio: decrease SHORT_PERIOD_MS to 6

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -128,7 +128,7 @@
 /* #define to use mmap no-irq mode for playback, #undef for non-mmap irq mode */
 #undef PLAYBACK_MMAP        // was #define
 /* short period (aka low latency) in milliseconds */
-#define SHORT_PERIOD_MS 11   // was 22
+#define SHORT_PERIOD_MS 6   // was 22
 /* deep buffer short period (screen on) in milliseconds */
 #define DEEP_BUFFER_SHORT_PERIOD_MS 22
 /* deep buffer long period (screen off) in milliseconds */
